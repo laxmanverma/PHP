d02daa76bf160a06f8ed16345e312fda24fc2b9e
laxmanverma <laxmannsit@gmail.com>
2017-07-20 11:06:31 +0530

    initial commit

---
+++ get.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<a href="get.php?subject=PHP&web=HOME">Test $GET</a>
+
+
+<?php 
+echo "Study " . $_GET['subject'] . " at " . $_GET['web'];
+?>
+
+</body>
+</html>
